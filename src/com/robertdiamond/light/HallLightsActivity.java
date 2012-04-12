3c82658aec595ecf0eef4894307156a87725dfe5
Alvaro Pereda <alvaro@pereda.me>
2012-04-13 00:25:48 +0100

    Project creation.

---
+++ src/com/robertdiamond/light/HallLightsActivity.java
@@ -0,0 +1,45 @@
+package com.robertdiamond.light;
+
+import java.util.Enumeration;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.digi.addp.AddpClient;
+import com.digi.addp.AddpDevice;
+import com.digi.addp.AddpDeviceList;
+
+public class HallLightsActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		
+		AddpClient addpClient = new AddpClient();
+
+		if (addpClient.SearchForDevices()) {
+			AddpDeviceList deviceList = addpClient.getDevices();
+
+			Enumeration<AddpDevice> e = deviceList.elements();
+			while (e.hasMoreElements()) {
+				AddpDevice device = e.nextElement();
+
+				// do something with the device here
+				System.out.println(device.toString());
+
+				// if device is not configured for DHCP, then turn it on and
+				// reboot.
+				if (device.getDHCP() == 0) {
+					addpClient.setDHCP(device, true, "dbps");
+					addpClient.rebootDevice(device, "dbps");
+				}
+			}
+		}
+	}
+}
